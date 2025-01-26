70441cf3f98c1353786df2b6e77b21b6ee1a1718
IgnacioTomasRamallo <ignacioramalloherrera@gmail.com>
2025-01-26 19:54:04 -0300

    Resolviendo rutas

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./components/Home/HomePage.tsx";
-import Movies from "./components/Movies/Movies.tsx";
+import { BrowserRouter } from "react-router";
+import Router from "./routes/index.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<Movies />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   </QueryClientProvider>
 );
